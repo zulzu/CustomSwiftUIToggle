d1080e2a622cde49ba4b199ad5517959736f8552
Andras Pal <andr4s.pal@gmail.com>
2021-01-06 10:54:32 +0000

    Initial Commit

---
+++ CustomSwiftUIToggle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSwiftUIToggle
+//
+//  Created by Andras Pal on 06/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSwiftUIToggle/CustomSwiftUIToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUIToggleApp.swift
+//  CustomSwiftUIToggle
+//
+//  Created by Andras Pal on 06/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUIToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
